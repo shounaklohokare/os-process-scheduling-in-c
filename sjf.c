ad7f0dcf4641755e71942944360812dde1730be4
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2022-01-26 12:05:55 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,190 +1,148 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX 20
 
-struct process
+typedef struct PROCESS
 {
-    int pid;
-    int at;
-    int bt;
-    int tat;
-    int ct;
-    int wt;
-};
+	int pid, tat, wt, at, bt, rt;
+} PROCESS;
 
-struct process p[MAX];
-int cur = 0;
+PROCESS p[10];
+int processCount, i, j, k, totalTat = 0, totalWt = 0;
+float avgtat, avgwt;
 
-float sum_tat = 0.0;
-float sum_wt = 0.0;
-
-void getPCB(int n)
+void sort()
 {
-
-    int i;
-
-    printf("\nEnter the details of process: ");
-    for (i = 0; i < n; i++)
-    {
-
-        printf("\nProcess---->%d", i + 1);
-
-        printf("\nEnter the arrival time of process: ");
-        scanf("%d", &p[i].at);
-
-        p[i].pid = i + 1;
-
-        printf("\nEnter the burst time of process: ");
-        scanf("%d", &p[i].bt);
-    }
+	PROCESS p1, p2;
+	for (i = 0; i < processCount; i++)
+	{
+		for (j = i + 1; j < processCount; j++)
+		{
+			if (p[j].at < p[i].at)
+			{
+				p1 = p[j];
+				p[j] = p[i];
+				p[i] = p1;
+			}
+			else if (p[j].bt < p[i].bt && p[j].at == p[i].at)
+			{
+				p2 = p[j];
+				p[j] = p[i];
+				p[i] = p2;
+			}
+		}
+	}
 }
 
-void putPCB(int n)
+void getProcess()
 {
+	printf("Enter the number of processes : ");
+	scanf("%d", &processCount);
+	for (i = 0; i < processCount; i++)
+	{
 
-    int i;
+		p[i].pid = i + 1;
 
-    printf("\nPID\tWT\tRT\tTAT");
-    for (i = 0; i < n; i++)
-    {
-        printf("\nP%d\t%d\t%d\t%d", p[i].pid, p[i].wt, p[i].wt, p[i].tat);
-    }
+		printf("Enter the arrival time : ");
+		scanf("%d", &p[i].at);
+
+		printf("Enter the burst time : ");
+		scanf("%d", &p[i].bt);
+
+		printf("\n");
+	}
+	sort();
 }
 
-void sortAT(int n)
+void process()
 {
-
-    int i, j;
-
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-        {
-
-            if (p[i].at > p[j].at)
-            {
-                struct process temp;
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
-        }
-    }
+	int time = 0, i = 0, k = 1, min, sum = 0, sum1 = 0;
+	PROCESS temp1;
+	printf("\nGanttChart : ");
+	printf("\n");
+	printf("|%d ", time);
+	for (j = 0; j < processCount; j++)
+	{
+		time = time + p[j].bt;
+		min = p[k].bt;
+		for (i = k; i < processCount; i++)
+		{
+			if (time >= p[i].at && p[i].bt < min)
+			{
+				temp1 = p[k];
+				p[k] = p[i];
+				p[i] = temp1;
+			}
+		}
+		k++;
+		printf("P%d ", p[j].pid);
+		if (j == processCount - 1)
+		{
+			printf("%d|", time);
+		}
+		else
+		{
+			printf("%d|%d ", time, time);
+		}
+	}
+	for (i = 1; i < processCount; i++)
+	{
+		sum = sum + p[i - 1].bt;
+		p[i].wt = p[i].rt = sum - p[i].at;
+		totalWt = totalWt + p[i].wt;
+	}
+	for (i = 0; i < processCount; i++)
+	{
+		p[i].tat = p[i].wt + p[i].bt;
+		totalTat = totalTat + p[i].tat;
+	}
+	printf("\n");
+	avgtat = (float)totalTat / processCount;
+	avgwt = (float)totalWt / processCount;
 }
 
-void sortPID(int n)
+void display()
 {
-
-    int i, j;
-
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-        {
-
-            if (p[i].pid > p[j].pid)
-            {
-                struct process temp;
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
-        }
-    }
+	printf("\n");
+	printf("Pid\tBT\tAT\tRT\tWT\tTAT\n");
+	for (i = 0; i < processCount; i++)
+	{
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].bt, p[i].at, p[i].rt, p[i].wt, p[i].tat);
+	}
+	printf("\n");
+	printf("Average Response time is %f \n", avgwt);
+	printf("Average Waiting time is %f \n", avgwt);
+	printf("Average Turnaround time is %f \n", avgtat);
 }
-
-int getSJ(int n)
+main()
 {
-
-    int i, j, min = 1000;
-    for (i = 0; i < n; i++)
-    {
-
-        if (p[i].ct == 0 && p[i].bt < min && p[i].at <= cur)
-        {
-            min = p[i].bt;
-            j = i;
-        }
-    }
-
-    return j;
+	getProcess();
+	process();
+	display();
 }
+/*
+Enter the number of processes : 4
+Enter the arrival time : 0
+Enter the burst time : 2
 
-void setGantt(int n)
-{
+Enter the arrival time : 2
+Enter the burst time : 3
 
-    int i, sjpos;
+Enter the arrival time : 2
+Enter the burst time : 4
 
-    for (i = 0; i < n; i++)
-    {
+Enter the arrival time : 3
+Enter the burst time : 3
 
-        if (p[i].at > cur)
-        {
 
-            p[i].ct = p[i].at + p[i].bt;
+GanttChart :
+|0 P1 2|2 P2 5|5 P4 8|8 P3 12|
 
-            cur = p[i].ct;
+Pid     BT      AT      RT      WT      TAT
+1       2       0       0       0       2
+2       3       2       0       0       3
+4       3       3       2       2       5
+3       4       2       6       6       10
 
-            p[i].tat = p[i].ct - p[i].at;
-            p[i].wt = p[i].tat - p[i].bt;
-        }
-        else
-        {
-
-            sjpos = getSJ(n);
-
-            p[sjpos].ct = cur + p[sjpos].bt;
-
-            cur = p[sjpos].ct;
-
-            p[sjpos].tat = p[sjpos].ct - p[sjpos].at;
-            p[sjpos].wt = p[sjpos].tat - p[sjpos].bt;
-        }
-    }
-}
-
-void calculateAvgTat(int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        sum_tat += p[i].tat;
-    }
-
-    printf("\nAverage TAT = %f", (float)sum_tat / n);
-}
-
-void calculateAvgWt(int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        sum_wt += p[i].wt;
-    }
-
-    printf("\nAverage WT = %f", (float)sum_wt / n);
-}
-
-int main()
-{
-
-    int i, n;
-
-    printf("\nEnter the number of process you want: ");
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++)
-    {
-        p[i].ct = 0;
-    }
-
-    getPCB(n);
-    sortAT(n);
-    setGantt(n);
-    sortPID(n);
-    putPCB(n);
-    calculateAvgTat(n);
-    calculateAvgWt(n);
-
-    return 0;
-}
+Average Response time is 2.000000
+Average Waiting time is 2.000000
+Average Turnaround time is 5.000000
+*/
